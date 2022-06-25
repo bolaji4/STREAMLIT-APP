4bb1ea266b0fa5b936dfe6f414e85278e689bec5
Olaniran Bolaji <45391374+bolaji4@users.noreply.github.com>
2022-06-25 15:28:42 +0100

    Update africafoodprices.py

--- africafoodprices.py
+++ africafoodprices.py
@@ -1,7 +1,10 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-import matplotlip.pyplot as plt
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import altair as alt
 
 print('installation is ready')
